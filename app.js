7c2344ae13e2968b74d84bcf59cdb322af9e19d4
Lime5005 <zlp5005@gmail.com>
2020-09-18 18:11:55 +0200

    Add some console functions

---
+++ app.js
@@ -0,0 +1,16 @@
+console.log('hello');
+console.log(true);
+console.log(123);
+console.log([1, 2, 3, 4]);
+console.log({ a: 1, b: 2 });
+console.table({ a: 1, b: 2 });
+console.error('this is an error');
+console.clear();
+console.warn('this is a warn');
+//to see how long some program takes:
+console.time('hello');
+console.log('hello');
+console.log('hello');
+console.log('hello');
+console.log('hello');
+console.timeEnd('hello'); //hello: 0.17822265625 ms
